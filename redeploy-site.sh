b3d8d01a69fddc2b41d56257899fb83240e5503e
Kayla Fang <kayfang182@gmail.com>
2022-06-25 20:26:59 -0400

    updated redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-tmux kill-server
 cd MLH
 cd project-melodramatic-monkeys
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new -d -s sess
-sleep 1
-tmux send -t sess 'flask run --host=0.0.0.0' Enter
+systemctl daemon-reload
+systemctl restart myportfolio
