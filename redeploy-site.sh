f6ec4a2b453f6851c77e61ea3836b5c28aa6bc60
Kayla Fang <kayfang182@gmail.com>
2022-07-11 06:13:20 -0400

    updated redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,8 +3,5 @@
 cd MLH
 cd project-melodramatic-monkeys
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
